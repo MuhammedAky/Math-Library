78d5f13c60ec0a41ad5ed5234db1021483a21ede
MuhammedAky <muhammedaky24@gmail.com>
2022-09-01 10:34:51 +0300

    Djkstra's Algorithm

--- test/main.c
+++ test/main.c
@@ -48,45 +48,29 @@ int main()
 {
     printf("Hello, world!\n");
 
-    // bigint b1 = strToBigint("293187123947883712964819237");
-    // bigint b2 = strToBigint("123987040123875132908741");
+    graph g = graph_new(ADJ_MATRIX, 7);
 
-    // bigint b3 = multiplyBigint(b1, b2);
-
-    // printf("%s * %s = %s\n", bigintToString(b1), bigintToString(b2), bigintToString(b3));
-
-    // freeBigint(&b1);
-    // freeBigint(&b2);
-    // freeBigint(&b3);
-
-    int N = 5;
-    graph g = graph_new(ADJ_MATRIX, N);
-
-    graph_addDirectedWeightedEdge(&g, 0, 1, 5);
-    graph_addUndirectedWeightedEdge(&g, 0, 2, 13);
-    graph_addDirectedEdge(&g, 2, 3);
-    graph_addDirectedWeightedEdge(&g, 3, 1, 6);
-    graph_addDirectedWeightedEdge(&g, 3, 4, 78);
+    graph_addDirectedWeightedEdge(&g, 0, 1, 6);
+    graph_addDirectedWeightedEdge(&g, 0, 3, 5);
+    graph_addDirectedWeightedEdge(&g, 1, 2, 7);
+    graph_addDirectedWeightedEdge(&g, 1, 3, 3);
+    graph_addDirectedWeightedEdge(&g, 2, 4, 15);
+    graph_addDirectedWeightedEdge(&g, 2, 5, 2);
+    graph_addDirectedWeightedEdge(&g, 3, 2, 2);
+    graph_addDirectedWeightedEdge(&g, 3, 4, 11);
+    graph_addDirectedWeightedEdge(&g, 4, 6, 3);
+    graph_addDirectedWeightedEdge(&g, 5, 4, 1);
+    graph_addDirectedWeightedEdge(&g, 5, 6, 6);
 
     char *str = graph_toString(&g);
     printf("%s\n", str);
     free(str);
 
-    int *d = malloc(N * sizeof(int));
-    int *f = malloc(N * sizeof(int));
-    int *p = malloc(N * sizeof(int));
-
-    graph_dfsStart(&g, 0, d, f, p);
+    int *p = graph_dijkstra(&g, 0);
+    printIntArr(p, g.n);
+    free(p);
 
     graph_free(&g);
-
-    printIntArr(d, N);
-    printIntArr(f, N);
-    printIntArr(p, N);
-
-    free(d);
-    free(f);
-    free(p);
 
     return 0;
 }
